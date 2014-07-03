0adccb519acf015f61a5dc451137a22468e4af5d
Harvey Xia <harvey@harveyxia.com>
2014-07-03 13:26:15 -0400

    Click next button to retrieve all prices

--- gflights.py
+++ gflights.py
@@ -1,6 +1,7 @@
 # from bs4 import BeautifulSoup
 import time
 import pdb
+from pprint import pprint
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 
@@ -10,7 +11,22 @@ driver = webdriver.Chrome(executable_path='/cygdrive/c/Python27/Scripts/chromedr
 GRAPH_PATH = '(//tbody)[2]/tr/td[2]/div/div[1]/div[4]/div[3]/div[2]/div[1]/div[2]'
 
 def main(f, t):
-    get_prices(f, t, '2014-07-02', '2014-07-06')
+    start = time.time()
+    prices = {}
+    url = build_url(f, t, '2014-07-02', '2014-07-06')
+    driver.get(url)
+    # select bar graph div
+    graph = wait_for_load(driver, GRAPH_PATH, 10)
+    # select next button
+    next =  graph.find_elements_by_xpath('../*')[5]
+    for x in range(6):
+        get_prices(graph, prices, f, t)
+        next.click()
+        next.click()
+        graph = wait_for_load(driver, GRAPH_PATH, 10)
+
+    pprint(prices)
+    print("It took %s seconds" % (time.time() - start))
 
 # waits for AJAX bar graph to load, given # of tries and .3s delay between each
 def wait_for_load(driver, x_path, tries):
@@ -36,12 +52,8 @@ def build_url(f, t, d, r):
 # driver.get('https://www.google.com/flights/#search;f=BOS;t=CIA,FCO;d=2014-07-02;r=2014-07-06;mc=p')
 
 # Args: f=place from, t=place to, d=departure date , r=return date
-def get_prices(f, t, d, r):
-    start = time.time()
-    url = build_url(f, t, d, r)
-
-    driver.get(url)
-    graph = wait_for_load(driver, GRAPH_PATH, 10)
+def get_prices(graph, prices, f, t):
+    
 
     bars = graph.find_elements_by_xpath('./*')
 
@@ -51,9 +63,9 @@ def get_prices(f, t, d, r):
         while date(graph) == 'Loading...':
             time.sleep(0.5)
             hov.perform()
-        print('Data: ' + date(graph) + ' ' + price(graph))
-
-    print("It took %s seconds" % (time.time() - start))
+        if date(graph) not in prices:
+            prices[date(graph)] = price(graph)
+        # print('Data: ' + date(graph) + ' ' + price(graph))
 
 def date(graph):
     return graph.find_elements_by_xpath('./*[last()-1]/*')[0].get_attribute('innerText')
